2fa84e60cc88086808e185c062fb78b6516ba8fb
samuelmayna <samuelmayna@gmail.com>
2022-01-09 10:06:29 +0300

    Added  a function that can be used  find the overall max by getting all the possible combinations of the three types for the 12 nodes

--- p2.js
+++ p2.js
@@ -10,3 +10,59 @@ let max = {
   //incase there are more than one arrangements that produce the max value
   arrangements: [],
 };
+
+const findOverallMax = async () => {
+  try {
+    for (const type1 of types) {
+      for (const type2 of types) {
+        for (const type3 of types) {
+          for (const type4 of types) {
+            for (const type5 of types) {
+              for (const type6 of types) {
+                for (const type7 of types) {
+                  for (const type8 of types) {
+                    for (const type9 of types) {
+                      for (const type10 of types) {
+                        for (const type11 of types) {
+                          for (const type12 of types) {
+                            const arrangement = [
+                              type1,
+                              type2,
+                              type3,
+                              type4,
+                              type5,
+                              type6,
+                              type7,
+                              type8,
+                              type9,
+                              type10,
+                              type11,
+                              type12,
+                            ];
+                            const foundMax = await P1(arrangement);
+                            counter++;
+                            if (Number(foundMax) > Number(max.currentMax)) {
+                              max.currentMax = foundMax;
+                              max.arrangements = [arrangement];
+                            } else if (
+                              Number(foundMax) === Number(max.currentMax)
+                            ) {
+                              max.arrangements.push(arrangement);
+                            }
+                            console.log(counter);
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
